abfdc4cb8c4b3f99fb65e719bfc216b909d528b1
Sahil Talwar <97950281+stalwar02@users.noreply.github.com>
2022-01-18 21:42:26 +0530

    Create Swapmeth1.cpp

---
+++ Swapmeth1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=100,b=200,c;
+    c=a;
+    a=b;
+    b=c;
+    cout<<"The Value of a is"<<a<<"The Value of b is:"<<b;
+    return 0;
+}

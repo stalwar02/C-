6c5ad7d2f726698bd9862360c0792f31d82858f4
Sahil Talwar <97950281+stalwar02@users.noreply.github.com>
2022-01-18 21:53:42 +0530

    Create ADD.cpp

---
+++ ADD.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter First Num: ";
+    cin>>a;
+    cout<<"Enter Second Num: ";
+    cin>>b;
+    c=a+b;
+    cout<<"Sum of Two Number is "<<c;
+}

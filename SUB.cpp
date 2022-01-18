accd44696fddd7ff30d24a89e54227fe9202412b
Sahil Talwar <97950281+stalwar02@users.noreply.github.com>
2022-01-18 21:54:28 +0530

    Create SUB.cpp

---
+++ SUB.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter First Num: ";
+    cin>>a;
+    cout<<"Enter Second Num: ";
+    cin>>b;
+    c=a-b;
+    cout<<"Subtraction of Two Number is "<<c;
+}

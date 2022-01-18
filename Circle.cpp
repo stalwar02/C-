2fb30205932b1f7fd307a01cfba43e05a26977a3
Sahil Talwar <97950281+stalwar02@users.noreply.github.com>
2022-01-18 21:51:51 +0530

    Update Circle.cpp

--- Circle.cpp
+++ Circle.cpp
@@ -1,11 +1,10 @@
 #include<iostream>
 using namespace std;
+#define Pi 3.14
 int main(){
-    float len,bre,c;
-    cout<<"The length of Triangle: ";
-    cin>>len;
-    cout<<"The Breadth of Triangle: ";
-    cin>>bre;
-    c=len*bre;
+    float r,c;
+    cout<<"The Radius of Circle: ";
+    cin>>r;
+    c=Pi*r*r;
     cout<<"The Area of Triangle is "<<c;
 }

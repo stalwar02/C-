faac76408bd88d94f86cde4cddfd7daddb86f82c
Sahil Talwar <97950281+stalwar02@users.noreply.github.com>
2022-01-18 21:45:20 +0530

    Create Swapmeth2.cpp

---
+++ Swapmeth2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=100,b=200;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout<<"The Value of a is"<<a<<"The Value of b is:"<<b;
+    return 0;
+}

96700235b8f8ed5dcd84a386ae898226b8a58879
Sahil Talwar <97950281+stalwar02@users.noreply.github.com>
2022-02-13 13:33:21 +0530

    Revert "Goto statement"

---
+++ Circle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+#define Pi 3.14
+int main(){
+    float r,c;
+    cout<<"The Radius of Circle: ";
+    cin>>r;
+    c=Pi*r*r;
+    cout<<"The Area of Triangle is "<<c;
+}
